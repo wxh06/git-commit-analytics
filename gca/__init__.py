8febe6f1efc5dbfb9dc46e4b3d10de5d9a8b3d2f
汪心禾 <wangxinhe06@gmail.com>
2020-06-21 18:41:59 +0800

    def committed_datetime

--- gca/__init__.py
+++ gca/__init__.py
@@ -5,10 +5,16 @@ import sys
 import git
 
 
+def committed_datetime(commits):
+    'commit.committed_datetime'
+    for commit in commits:
+        yield commit.committed_datetime
+
+
 def main(repo, branch):
     "__name__ == '__main__'"
-    for commit in git.Repo(repo).iter_commits(branch):
-        print(commit.committed_datetime)
+    for c_dt in committed_datetime(git.Repo(repo).iter_commits(branch)):
+        print(c_dt)
 
 
 if __name__ == '__main__':
