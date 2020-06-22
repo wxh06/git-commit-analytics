728574a19458dee1f39016f4c89ceb1dcef87146
汪心禾 <wangxinhe06@gmail.com>
2020-06-22 11:52:46 +0800

    Commits per day hour

--- gca/__init__.py
+++ gca/__init__.py
@@ -1,5 +1,7 @@
 'Git Commit Analytics'
 
+from math import ceil
+import shutil
 import sys
 
 import git
@@ -11,11 +13,30 @@ def committed_datetime(commits):
         yield commit.committed_datetime
 
 
-def main(repo, branch):
+def hour(commits):
+    'Commits per day hour'
+    hours = [
+        {'insertions': 0, 'deletions': 0, 'files': 0, 'lines': 0}
+        for i in range(24)
+    ]
+    for commit in commits:
+        for stat in hours[commit.committed_datetime.hour].keys():
+            hours[commit.committed_datetime.hour][stat] \
+                += commit.stats.total[stat]
+    return hours
+
+
+def main(repo, branch, author):
     "__name__ == '__main__'"
-    for c_dt in committed_datetime(git.Repo(repo).iter_commits(branch)):
-        print(c_dt)
+    commits = git.Repo(repo).iter_commits(branch, author=author)
+    hours = hour(commits)
+    max_lines = max(i['lines'] for i in hours)
+    terminal_size = shutil.get_terminal_size((80, 20))
+    ratio = (terminal_size.columns - 6) / max_lines
+    for i, cnt in zip(range(24), hours):
+        print(f"{i:2}: \x1b[32m{'+' * ceil(cnt['insertions'] * ratio)}"
+              f"\x1b[31m{'-' * ceil(cnt['deletions'] * ratio)}\x1b[0m")
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], 'master')
+    main(*sys.argv[1:])
